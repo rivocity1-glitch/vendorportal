3716b5da9ead75101d8fcc74d1abbc53aa658bf3
Rivo <rivo.city1@gmail.com>
2026-09-08 12:46:01 +0530

    Apply product thumbnail URL normalization

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+// @ts-ignore: allow side-effect CSS import without type declarations
+import "./styles/index.css";
+import "./lib/normalizeProductImageSrc";
+import "./i18n";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  // @ts-ignore: allow side-effect CSS import without type declarations
-  import "./styles/index.css";
-  import './i18n';
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
